2612f6131379c36cf2da4836adfa18886155bb2d
BytejunkieX <mike.quade@me.com>
2013-07-27 12:28:28 +0200

    Initial Commit

---
+++ iOS-Test4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Test4
+//
+//  Created by Mike Quade on 27.07.13.
+//  Copyright (c) 2013 Mike Quade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS-Test4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS-Test4
+//
+//  Created by Mike Quade on 27.07.13.
+//  Copyright (c) 2013 Mike Quade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
